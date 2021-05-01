c1d9596410ed1bec3a0161bc9538a8f204c8471d
stivenz <1806@holbertonschool.com>
2021-05-01 15:20:29 -0500

    Display route with variable input

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """Route definition"""
+    app.url_map.strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """Displays HBNB"""
+    app.url_map.strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def replace_var(text):
+    """Replaces an input variable"""
+    new_text = text.replace('_', ' ')
+    app.url_map.strict_slashes = False
+    return ('C %s' % new_text)
+
+
+if __name__ == '__main__':
+    app.run(port='5000', host='0.0.0.0')
